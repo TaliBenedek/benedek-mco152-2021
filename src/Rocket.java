77b5685150794a25f6e206d0981562db87e33c28
Tali211 <58050504+Tali211@users.noreply.github.com>
2021-02-02 18:56:01 -0500

    Added comments to Rocket

--- src/Rocket.java
+++ src/Rocket.java
@@ -9,6 +9,10 @@ public class Rocket {
         this.angle = Math.toRadians(angle);
     }
 
+    /**
+    * @param time in seconds
+    * @return x position of rocket at the specified time
+    */
     public double getX(double time)
     {
         double xVelocity = Math.cos(angle) * velocity;
@@ -16,6 +20,10 @@ public class Rocket {
         return x;
     }
 
+    /**
+     * @param time in seconds
+     * @return y position of rocket at the specified time
+     */
     public double getY(double time)
     {
         double yVelocity = Math.sin(angle) * velocity;
@@ -23,6 +31,10 @@ public class Rocket {
         return y;
     }
 
+    /**
+     *
+     * @return the flight time of the rocket in seconds
+     * */
     public double getFlightTime()
     {
         double yVelocity = Math.sin(angle) * velocity;
