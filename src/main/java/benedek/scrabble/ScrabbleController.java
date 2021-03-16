3014661e059b9f183b7b1d0b61e52df69cf814fd
TaliBenedek <58050504+Tali211@users.noreply.github.com>
2021-03-16 17:11:52 -0400

    Added ScrabbleController

--- src/main/java/benedek/scrabble/ScrabbleController.java
+++ src/main/java/benedek/scrabble/ScrabbleController.java
@@ -1,5 +1,148 @@
 package benedek.scrabble;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
 public class ScrabbleController
 {
+    @FXML
+    List<Label> answerLabels;
+    @FXML
+    List<Label> letterLabels;
+    @FXML
+    Label pointsLabel;
+
+    LetterBag letterBag = new LetterBag();
+    Dictionary dictionary;
+
+    public ScrabbleController() throws FileNotFoundException
+    {
+        try{
+            dictionary = new Dictionary();
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void initialize() {
+        for (Label label : letterLabels) {
+            label.setText(letterBag.nextLetter());
+        }
+    }
+
+    public void onLetterClicked(MouseEvent event)
+    {
+        Label letterLabel = (Label) event.getSource();
+        String letter = letterLabel.getText();
+        for(Label answerLabel: answerLabels)
+        {
+            if(answerLabel.getText().equals(""))
+            {
+                answerLabel.setText(letter);
+                letterLabel.setText("");
+                break;
+            }
+        }
+    }
+    public void onAnswerClicked(MouseEvent event)
+    {
+        Label label = (Label) event.getSource();
+        String letter = label.getText();
+        label.setText("");
+        for(Label letterLabel: letterLabels)
+        {
+            if(letterLabel.getText().equals(""))
+            {
+                letterLabel.setText(letter);
+                break;
+            }
+        }
+    }
+
+    public void onSubmit(ActionEvent event)
+    {
+        String word = "";
+        for(Label answerLabel: answerLabels)
+        {
+            word += answerLabel.getText();
+        }
+        if(dictionary.findWord(word))
+        {
+            int oldPoints = 0;
+            try
+            {
+                oldPoints = Integer.parseInt(pointsLabel.getText());
+            }
+            catch(Exception e)
+            {
+
+            }
+            int newPoints = getNumOfPoints(word);
+            pointsLabel.setText(String.valueOf(oldPoints + newPoints));
+            for(int index = 0; index < answerLabels.size(); index++)
+            {
+                answerLabels.get(index).setText("");
+                if(letterLabels.get(index).getText().equals("") && !letterBag.isEmpty())
+                {
+                    letterLabels.get(index).setText(letterBag.nextLetter());
+                }
+            }
+        }
+        else
+        {
+            onClear(null);
+        }
+    }
+
+    public void onClear(ActionEvent event)
+    {
+        String[] clearedLetters = new String[7];
+        for(int labelIndex = 0, lettersIndex = 0; labelIndex < clearedLetters.length; labelIndex++)
+        {
+            clearedLetters[labelIndex] = answerLabels.get(labelIndex).getText();
+            answerLabels.get(labelIndex).setText("");
+            if(letterLabels.get(labelIndex).getText().equals(""))
+            {
+                letterLabels.get(labelIndex).setText(clearedLetters[lettersIndex]);
+                lettersIndex++;
+            }
+        }
+    }
+
+    private int getNumOfPoints(String word)
+    {
+        int numOfLetters = word.length();
+        int points = 0;
+        switch (numOfLetters)
+        {
+            case 2:
+                points = 1;
+                break;
+            case 3:
+                points = 3;
+                break;
+            case 4:
+                points = 5;
+                break;
+            case 5:
+                points = 7;
+                break;
+            case 6:
+                points = 11;
+                break;
+            case 7:
+                points = 13;
+                break;
+            default:
+        }
+        return points;
+    }
 }
