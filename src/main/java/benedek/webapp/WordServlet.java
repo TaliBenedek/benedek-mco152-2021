b13327ad7844d39742d114db8243b49e940d429e
TaliBenedek <67157784+TaliBenedek@users.noreply.github.com>
2021-12-06 17:28:58 -0500

    Continued working on servlet and test

---
+++ src/main/java/benedek/webapp/WordServlet.java
@@ -0,0 +1,32 @@
+package benedek.webapp;
+
+import benedek.scrabble.Dictionary;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WordServlet
+{
+    private final Dictionary dictionary;
+
+    public WordServlet() throws IOException
+    {
+        dictionary = new Dictionary();
+    }
+
+    public void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException
+    {
+        PrintWriter out = response.getWriter();
+
+        String word = request.getParameter("word");
+        String definition = dictionary.getDefinition(word);
+        String output = (definition.equals("")) ? "Word not found" : definition;
+
+        out.println(output);
+    }
+}
